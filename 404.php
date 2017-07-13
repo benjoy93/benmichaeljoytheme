34cb2801a3fa51e46d4b6f493e0daa63060a9d73
Ben Joy <ben.joy1@sky.com>
2017-07-13 23:53:28 +0100

    add 404 page

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+ <div class="wrapper top-margin not-found">
+        <h1>Yikes!</h1>
+        <h2>There doesn't seem to be a page here.</h2>
+         
+
+        <h3 class="blog-content">Why not check out some of my <a href="/index.php?page_id=6">recent blog posts</a>, or just head on over to the <a href="/index.php?page_id=15">homepage</a> and try again.</h3>        
+
+</div>
+
+
+<?php get_footer(); ?>
